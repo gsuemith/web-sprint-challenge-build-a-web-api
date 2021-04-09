c082a2b98df42ba847ce75c3177fbcbfaa36bcb0
Garrick Suemith <garrick.suemith@gmail.com>
2021-04-09 05:30:02 -0400

    all endpoints working

--- api/server.js
+++ api/server.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const helmet = require('helmet')
+
+const actionsRouter = require('./actions/actions-router')
+const projectsRouter = require('./projects/projects-router')
+
 const server = express();
 
-// Complete your server here!
-// Do NOT `server.listen()` inside this file!
+// middlewares
+server.use(express.json())
+server.use(helmet())
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({ error: err.message,
+    message: "Something happened with the server"
+  })
+})
+
+// routers
+server.use('/api/actions', actionsRouter)
+server.use('/api/projects', projectsRouter)
+
+// defaults
+server.use("/api", (_, res) => {
+  res.json({ data: "API is accounted for" })
+})
+
+server.use('/', (req, res) => {
+  res.send(`<h2>Welcome to our API!</h2>`);
+})
 
 module.exports = server;
